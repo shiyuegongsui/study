e8ed7d48dfd5e9656235439fd9200c24dfa9ef27
shiyuegongsui <simply_love12@163.com>
2020-12-17 17:40:56 +0800

    增加跨域

--- 跨域详解/3.cors/index.js
+++ 跨域详解/3.cors/index.js
@@ -2,7 +2,7 @@ let express= require('express');
 let app=express();
 app.use(express.static(__dirname));
 
-app.get('/say',function(req,res){
+app.post('/say',function(req,res){
     res.end('我是3000的返回值');
 });
 app.listen(3000);
--- 跨域详解/3.cors/index2.js
+++ 跨域详解/3.cors/index2.js
@@ -4,20 +4,31 @@ app.use(express.static(__dirname));
 let whiteList=['http://127.0.0.1:3000'];
 
 
-app.post('/say',function(req,res){
-    let origin=req.headers.origin;
-    if(whiteList.includes(origin)){
-        res.setHeader('Access-Control-Allow-Origin',origin);
+//中间件
+app.use(function(req,res,next){
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Headers','*');
+    res.setHeader('Access-Control-Allow-Method','*');
+    if(req.method === 'OPTIONS'){
+        res.end();
     }
+    next();
+})
+
+app.post('/say',function(req,res){
+    // let origin=req.headers.origin;
+    // if(whiteList.includes(origin)){
+    //     res.setHeader('Access-Control-Allow-Origin',origin);
+    // }
     res.end('我是4000的返回值');
 });
 
 
 app.get('/say',function(req,res){
-    let origin=req.headers.origin;
-    if(whiteList.includes(origin)){
-        res.setHeader('Access-Control-Allow-Origin',origin);
-    }
+    // let origin=req.headers.origin;
+    // if(whiteList.includes(origin)){
+    //     res.setHeader('Access-Control-Allow-Origin',origin);
+    // }
     res.end('我是4000的返回值');
 });
 
