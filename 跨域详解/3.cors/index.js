112f8417919ce063ae9f601d0be61566c40b1a65
shiyuegongsui <simply_love12@163.com>
2021-02-03 17:13:57 +0800

    修改跨域

--- 跨域详解/3.cors/index.js
+++ 跨域详解/3.cors/index.js
@@ -2,6 +2,12 @@ let express= require('express');
 let app=express();
 app.use(express.static(__dirname));
 
+app.use(function(req,res,next){
+    res.setHeader('max-age','3600 public');
+    next();
+})
+
+
 app.post('/say',function(req,res){
     res.end('我是3000的返回值');
 });
--- 跨域详解/3.cors/index2.js
+++ 跨域详解/3.cors/index2.js
@@ -6,9 +6,11 @@ let whiteList=['http://127.0.0.1:3000'];
 
 //中间件
 app.use(function(req,res,next){
-    res.setHeader('Access-Control-Allow-Origin','*');
-    res.setHeader('Access-Control-Allow-Headers','*');
+    res.setHeader('Access-Control-Allow-Origin','http://127.0.0.1:3000');
+    res.setHeader('Access-Control-Allow-Headers','name');
     res.setHeader('Access-Control-Allow-Method','*');
+    // res.setHeader('Access-Control-Allow-Credentials',true);
+
     if(req.method === 'OPTIONS'){
         res.end();
     }
